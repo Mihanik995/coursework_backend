9bf1636f986cf1944fb5d2295d9927a0879194f5
user <user@example.com>
2023-12-08 20:05:59 +0300

    write utils and additional tests

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,33 @@
 import json
 import os.path
+from datetime import datetime
 
 
 def unpack_json():
-    ...
+    result = None
+    with open(os.path.join('..', 'operations.json'), 'r', encoding='utf-8') as file:
+        result = json.load(file)
+    return result
 
 
 def form_transaction(transaction):
-    ...
+    date_string = transaction['date']
+    date_string_format = datetime.strptime(date_string[:10], '%Y-%m-%d').date().strftime('%d.%m.%Y')
+
+    final_string = f"{date_string_format} {transaction['description']}\n"
+
+    if 'from' in transaction.keys():
+        final_string += form_address(transaction['from'])
+
+    final_string += f" -> {form_address(transaction['to'])}\n"
+
+    final_string += f"{transaction['operationAmount']['amount']} {transaction['operationAmount']['currency']['name']}"
+
+    return final_string
+
+
+def form_address(address):
+    if address.count('Счет'):
+        return f"Счет **{address[-4:]}"
+    else:
+        return f"{address[:-12]} {address[-12:-10]}** **** {address[-4:]}"
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,3 +10,8 @@ def test_form_transaction():
                                        0])) == "26.08.2019 Перевод организации\nMaestro 1596 83** **** 5199 -> Счет **9589\n31957.58 руб."
     assert (utils.form_transaction(
         utils.unpack_json()[3])) == "23.03.2018 Открытие вклада\n -> Счет **2431\n48223.05 руб."
+
+
+def test_form_address():
+    assert (utils.form_address("Счет 41421565395219882431")) == "Счет **2431"
+    assert (utils.form_address("MasterCard 7158300734726758")) == "MasterCard 7158 30** **** 6758"
